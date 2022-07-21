670d323d515bcebec7d8aa4a3d99ee67a3d50f87
rushdi <rushdi.hoosain@payu.co.za>
2022-07-21 14:54:29 +0100

    configured postgres db

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """add x and y and return result"""
+
+    return x + y
+
+def subtract(x, y):
+    """subtract x and y and return result"""
+
+    return x - y    
---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+"""
+sample tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module """
+
+    def test_add_numbers(self):
+        """test adding numbers together """
+        res = calc.add(5, 4)
+
+        self.assertEqual(res, 9)
+
+    def test_subtract_number(self):
+        """test subtracting numbers """
+        res = calc.subtract(15, 10)
+
+        self.assertEqual(res, 5)
+        
